3492b92d38f6c0f4f3caa1fc9932ffab5f9758d3
NetZach777 <150202716+NetZach777@users.noreply.github.com>
2024-08-18 11:58:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,15 @@ class TweetRequest(BaseModel):
 
 @app.post("/predict-sentiment/")
 def predict_sentiment(tweet: TweetRequest):
-    # Vérification que le texte n'est pas vide ou constitué d'espaces blancs
+    # Vérifie si le texte est vide ou ne contient que des espaces blancs
     if not tweet.text.strip():
         raise HTTPException(status_code=422, detail="Text cannot be empty or just whitespace.")
 
-    # Tokeniser l'entrée
+    # Tokenisation avec batch_size=1 pour une seule entrée
+    inputs = tokenizer([tweet.text], return_tensors="tf", max_length=512, truncation=True, padding="max_length")
+
+    # Effectuer la prédiction avec batch_size=1
     try:
-        inputs = tokenizer(tweet.text, return_tensors="tf", max_length=512, truncation=True, padding="max_length")
         outputs = model(inputs['input_ids'], attention_mask=inputs['attention_mask'])
         prediction = tf.argmax(outputs.logits, -1).numpy()[0]
     except Exception as e:
@@ -42,3 +44,4 @@ def predict_sentiment(tweet: TweetRequest):
     sentiment = "positive" if prediction == 1 else "negative"
     
     return {"sentiment": sentiment}
+
